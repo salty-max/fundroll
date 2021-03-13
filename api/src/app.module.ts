a937cfb92fcba01391a2459f9380dac10a462559
max-jellycat <max@jellycat.fr>
2021-03-14 00:46:09 +0100

    Added Http error filter + logging interceptor

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HttpErrorFilter } from './shared/http-error.filter';
+import { LoggingInterceptor } from './shared/logging.interceptor';
 import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), TransactionModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
