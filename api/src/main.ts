cefd3c976826fbf135151bd6eb9203a854068d1f
max-jellycat <max@jellycat.fr>
2021-03-13 23:02:35 +0100

    connected postgres db

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+
+const port = process.env.API_PORT || 1337;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.API_PORT);
+  await app.listen(port);
+  Logger.log(`Server running on port ${port}`, 'Boostrap');
 }
 bootstrap();
